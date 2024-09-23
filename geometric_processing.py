6b8e3d1e9fa82c41352778d2806bbd59f17b6844
Rishabh Singh <rishabhsingh@eduroam-141-23-175-27.wlan.tu-berlin.de>
2024-09-23 14:51:38 +0200

    Trying to run geometric

---
+++ geometric_processing.py
@@ -0,0 +1,95 @@
+import copy
+import os.path as osp
+import time
+import pickle
+import torch
+import torch.nn.functional as F
+from tqdm import tqdm
+from torch_geometric.data import HeteroData
+from torch_geometric.datasets import Reddit
+from torch_geometric.loader import NeighborLoader
+from torch_geometric.nn import SAGEConv, to_hetero
+from torch_geometric.utils.convert import to_networkx, from_networkx
+
+from torch_geometric.datasets import OGB_MAG
+
+sample_dataset = OGB_MAG(root='./data', preprocess='metapath2vec')
+sample_data = sample_dataset[0]
+
+
+hetero_data = HeteroData()
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+save_path = '/Users/rishabhsingh/Rishabh_thesis_code/Mails_Graph/saved_data/graph.pkl'
+with open(save_path, mode='rb') as f:
+    dataset = pickle.load(f)
+
+def get_nodetype_length(node_type):
+    length = len([n for n, attr in dataset.nodes(data=True) if attr.get('node_type') == node_type])
+    return length
+
+def get_edgetype_length(src_type, tgt_type, edge_type):
+    length = len([
+        1 for source, target, attr in dataset.edges(data=True)
+        if attr.get('edge_type') == edge_type
+        and dataset.nodes[source]['node_type'] == src_type
+        and dataset.nodes[target]['node_type'] == tgt_type
+    ])
+    return length
+
+pyg = from_networkx(dataset)
+hetero_data= pyg
+
+hetero_data['email'].x = [get_nodetype_length('email'), 384]
+hetero_data['adresse'].x = [get_nodetype_length('adresse'), 384]
+hetero_data['Betreff'].x = [get_nodetype_length('Betreff'), 384]
+hetero_data['name'].x = [get_nodetype_length('name'), 384]
+hetero_data['noun'].x = [get_nodetype_length('noun'), 384]
+hetero_data['context'].x = [get_nodetype_length('context'), 384]
+
+hetero_data['email', 'noun', 'belongs_to'].edge_index = [2, get_edgetype_length('email','noun','belongs_to')]
+hetero_data['email', 'adresse', 'Von: (Adresse)'].edge_index = [2, get_edgetype_length('email','adresse','Von: (Adresse)')]
+hetero_data['email', 'adresse', 'An: (Adresse)'].edge_index = [2, get_edgetype_length('email','adresse','An: (Adresse)')]
+hetero_data['email', 'Betreff', 'Betreff'].edge_index = [2, get_edgetype_length('email','Betreff','Betreff')]
+hetero_data['email', 'name', 'An: (Name)'].edge_index = [2, get_edgetype_length('email','name','An: (Name)')]
+hetero_data['email', 'name', 'Von: (Name)'].edge_index = [2, get_edgetype_length('email','name','Von: (Name)')]
+hetero_data['noun', 'context', 'has_context'].edge_index = [2, get_edgetype_length('noun','context','has_context')]
+hetero_data['noun', 'sentence', 'comes_before'].edge_index = [2, get_edgetype_length('noun','sentence','comes_before')]
+hetero_data['noun', 'sentence', 'comes_after'].edge_index = [2, get_edgetype_length('noun','sentence','comes_after')]
+
+hetero_data['email', 'noun', 'belongs_to'].edge_attr = [get_edgetype_length('email','noun','belongs_to'),1]
+hetero_data['email', 'adresse', 'Von: (Adresse)'].edge_attr = [get_edgetype_length('email','adresse','Von: (Adresse)'),1]
+hetero_data['email', 'adresse', 'An: (Adresse)'].edge_attr = [get_edgetype_length('email','adresse','An: (Adresse)'),1]
+hetero_data['email', 'Betreff', 'Betreff'].edge_attr = [get_edgetype_length('email','Betreff','Betreff'),1]
+hetero_data['email', 'name', 'An: (Name)'].edge_attr = [get_edgetype_length('email','name','An: (Name)'),1]
+hetero_data['email', 'name', 'Von: (Name)'].edge_attr = [get_edgetype_length('email','name','Von: (Name)'),1]
+hetero_data['noun', 'context', 'has_context'].edge_attr = [get_edgetype_length('noun','context','has_context'),1]
+hetero_data['noun', 'sentence', 'comes_before'].edge_attr = [get_edgetype_length('noun','sentence','comes_before'),1]
+hetero_data['noun', 'sentence', 'comes_after'].edge_attr = [get_edgetype_length('noun','sentence','comes_after'),1]
+
+
+node_types, edge_types = hetero_data.metadata()
+
+
+
+
+
+dataset.train_mask = torch.zeros(len(dataset.nodes), dtype=torch.bool)
+
+
+class GNN(torch.nn.Module):
+    def __init__(self, hidden_channels, out_channels):
+        super().__init__()
+        self.conv1 = SAGEConv((-1, -1), hidden_channels)
+        self.conv2 = SAGEConv((-1, -1), out_channels)
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index).relu()
+        x = self.conv2(x, edge_index)
+        return x
+
+
+model = GNN(hidden_channels=64, out_channels=2)
+model = to_hetero(model, hetero_data.metadata(), aggr='sum')
+
+print(model)
